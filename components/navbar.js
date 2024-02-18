76e0064cb692411ec67f9a852c68fcf40b9c596f
Ad <thenewadeel@gmail.com>
2024-02-18 22:30:23 +0500

    footer dne

--- components/navbar.js
+++ components/navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <Link href="/">
                   <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
-                      <Image src="/img/qcl.jpg" alt="N" width="32" height="32" className="w-8" />
+                      <Image src="/img/logo.svg" alt="N" width="32" height="32" className="w-8" />
                     </span>
                     <span>QCL</span>
                   </span>
