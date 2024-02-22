6a7a17f19bd9c4bda55e9713a4da2f127edfc711
Ad <thenewadeel@gmail.com>
2024-02-22 13:12:03 +0500

    contact form done

--- components/contact.js
+++ components/contact.js
@@ -6,69 +6,114 @@ const Contact = (props) => {
   const { data } = props;
   return (
     <>
-      <Container className="flex flex-wrap mb-20 lg:gap-10 lg:flex-nowrap ">
+      <Container className=" ">
         <div
           name="contact"
-          className={`flex items-center justify-center w-full lg:w-1/2 ${props.imgPos === "right" ? "lg:order-1" : ""}`}
+          className="grid md:grid-cols-2 gap-16 items-center relative overflow-hidden p-10 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-3xl max-w-7xl mx-auto bg-slate-100 text-gray-900 my-6 before:absolute before:right-0 before:w-[45%] before:bg-indigo-200 dark:before:bg-red-950 before:h-full max-md:before:hidden shadow-zinc-950 dark:bg-slate-950 dark:text-gray-300 dark:shadow-slate-600"
         >
           <div>
-            <Image
-              src={data.image}
-              width="521"
-              height="auto"
-              alt="Contact"
-              className={"object-cover"}
-              placeholder="blur"
-              blurDataURL={data.image.src}
-            />
-          </div>
-        </div>
+            <h2 className="text-3xl font-extrabold">Get In Touch</h2>
+            <p className="text-sm  mt-3">
+              Have a specific inquiry or looking to explore new opportunities? Our experienced team is ready to engage
+              with you.
+            </p>
+            <form netlify>
+              <div className="space-y-4 mt-8 ">
+                <input
+                  type="text"
+                  placeholder="Full Name"
+                  className="placeholder:text-gray-600 px-2 py-3 bg-slate-300 w-full text-sm border-b-4 focus:border-red-700 outline-none rounded-md transition-all duration-500 dark:text-black"
+                />
 
-        <div
-          className={`flex flex-wrap items-center w-full lg:w-1/2 ${data.imgPos === "right" ? "lg:justify-end" : ""}`}
-        >
-          <div>
-            <div className="flex flex-col w-full mt-4 justify-items-start">
-              <h3 className="max-w-2xl mt-3 text-3xl font-bold leading-snug tracking-tight text-gray-800 lg:leading-tight lg:text-4xl dark:text-white">
-                Contact Us
-              </h3>
-              <p className="max-w-2xl py-4 text-lg leading-normal text-gray-500 lg:text-xl xl:text-xl dark:text-gray-300">
-                Your feedback is important to us
-              </p>
-              <form
-                name="contact"
-                method="POST"
-                data-netlify="true"
-                className="z-10 flex flex-col justify-around rounded-xl text-ellipsis h-full p-4 text-justify text-base text-white relative md:relative md:text-black leading-4 inset-x-0 md:text-2xl md:indent-16 bg-black bg-opacity-80 md:bg-opacity-60 md:shadow-inner"
-              >
-                <div className="flex row p-2 justify-between">
+                <input
+                  type="text"
+                  placeholder="City"
+                  className="placeholder:text-gray-600 px-2 py-3 bg-slate-300 w-full text-sm border-b-4 focus:border-red-700 outline-none rounded-md transition-all duration-500 dark:text-black"
+                />
+                <div className="grid grid-cols-2 gap-6">
                   <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    className="mx-4 rounded-md focus:border-opacity-80 border-4 border-red-300 border-opacity-0 text-black"
+                    type="number"
+                    placeholder="Phone No."
+                    className="placeholder:text-gray-600 px-2 py-3 bg-slate-300 w-full text-sm border-b-4 focus:border-red-700 outline-none rounded-md transition-all duration-500 dark:text-black"
                   />
-                </div>
-                <div className="flex row p-2 justify-start">
+
                   <input
                     type="email"
-                    name="email"
-                    placeholder="eMail address"
-                    className="mx-4 rounded-md focus:border-opacity-80 text-black border-2 border-red-300 border-opacity-0"
+                    placeholder="Email"
+                    className="placeholder:text-gray-600 px-2 py-3 bg-slate-300 w-full text-sm border-b-4 focus:border-red-700 outline-none rounded-md transition-all duration-500 dark:text-black"
                   />
                 </div>
-                <div className="flex row p-2 justify-start items-center">
-                  <textarea
-                    name="message"
-                    placeholder="Your Message will be directed to Club management"
-                    className="mx-4 rounded-md focus:border-opacity-80 text-black border-2 border-red-300 border-opacity-0"
-                  ></textarea>
-                </div>
-                <button className="bg-red-300 hover:bg-red-600 font-bold hover:text-white hover:shadow-md transition-all rounded-xl p-4 px-6 m-4 text-black text-xl self-stretch">
-                  Submit
-                </button>
-              </form>
-            </div>
+                <textarea
+                  placeholder="Write Message"
+                  className="placeholder:text-gray-600 px-2 py-3 bg-slate-300 w-full text-sm border-b-4 focus:border-red-700 outline-none rounded-md transition-all duration-500 h-32 dark:text-black"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="mt-8 flex items-center justify-center text-sm w-full px-4 py-2.5 font-semibold hover:bg-red-950 outline-none border-2 focus:border-gray-950  text-white bg-red-900 dark:bg-red-950 dark:border-2 dark:border-gray-300 dark:text-gray-300 rounded-md "
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16px"
+                  height="16px"
+                  fill="#fff"
+                  className="mr-2"
+                  viewBox="0 0 548.244 548.244"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M392.19 156.054 211.268 281.667 22.032 218.58C8.823 214.168-.076 201.775 0 187.852c.077-13.923 9.078-26.24 22.338-30.498L506.15 1.549c11.5-3.697 24.123-.663 32.666 7.88 8.542 8.543 11.577 21.165 7.879 32.666L390.89 525.906c-4.258 13.26-16.575 22.261-30.498 22.338-13.923.076-26.316-8.823-30.728-22.032l-63.393-190.153z"
+                    clip-rule="evenodd"
+                    data-original="#000000"
+                  />
+                </svg>
+                Send Message
+              </button>
+            </form>
+            <ul className="mt-4 flex justify-center lg:space-x-6 max-lg:flex-col max-lg:items-center max-lg:space-y-2 ">
+              <li className="flex items-center hover:text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16px"
+                  height="16px"
+                  fill="currentColor"
+                  viewBox="0 0 479.058 479.058"
+                >
+                  <path
+                    d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
+                    data-original="#000000"
+                  />
+                </svg>
+                <a href="javascript:void(0)" className="text-current text-sm ml-3">
+                  <strong>info@quettaclub.org</strong>
+                </a>
+              </li>
+              <li className="flex items-center text-current hover:text-blue-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16px"
+                  height="16px"
+                  fill="currentColor"
+                  viewBox="0 0 482.6 482.6"
+                >
+                  <path
+                    d="M98.339 320.8c47.6 56.9 104.9 101.7 170.3 133.4 24.9 11.8 58.2 25.8 95.3 28.2 2.3.1 4.5.2 6.8.2 24.9 0 44.9-8.6 61.2-26.3.1-.1.3-.3.4-.5 5.8-7 12.4-13.3 19.3-20 4.7-4.5 9.5-9.2 14.1-14 21.3-22.2 21.3-50.4-.2-71.9l-60.1-60.1c-10.2-10.6-22.4-16.2-35.2-16.2-12.8 0-25.1 5.6-35.6 16.1l-35.8 35.8c-3.3-1.9-6.7-3.6-9.9-5.2-4-2-7.7-3.9-11-6-32.6-20.7-62.2-47.7-90.5-82.4-14.3-18.1-23.9-33.3-30.6-48.8 9.4-8.5 18.2-17.4 26.7-26.1 3-3.1 6.1-6.2 9.2-9.3 10.8-10.8 16.6-23.3 16.6-36s-5.7-25.2-16.6-36l-29.8-29.8c-3.5-3.5-6.8-6.9-10.2-10.4-6.6-6.8-13.5-13.8-20.3-20.1-10.3-10.1-22.4-15.4-35.2-15.4-12.7 0-24.9 5.3-35.6 15.5l-37.4 37.4c-13.6 13.6-21.3 30.1-22.9 49.2-1.9 23.9 2.5 49.3 13.9 80 17.5 47.5 43.9 91.6 83.1 138.7zm-72.6-216.6c1.2-13.3 6.3-24.4 15.9-34l37.2-37.2c5.8-5.6 12.2-8.5 18.4-8.5 6.1 0 12.3 2.9 18 8.7 6.7 6.2 13 12.7 19.8 19.6 3.4 3.5 6.9 7 10.4 10.6l29.8 29.8c6.2 6.2 9.4 12.5 9.4 18.7s-3.2 12.5-9.4 18.7c-3.1 3.1-6.2 6.3-9.3 9.4-9.3 9.4-18 18.3-27.6 26.8l-.5.5c-8.3 8.3-7 16.2-5 22.2.1.3.2.5.3.8 7.7 18.5 18.4 36.1 35.1 57.1 30 37 61.6 65.7 96.4 87.8 4.3 2.8 8.9 5 13.2 7.2 4 2 7.7 3.9 11 6 .4.2.7.4 1.1.6 3.3 1.7 6.5 2.5 9.7 2.5 8 0 13.2-5.1 14.9-6.8l37.4-37.4c5.8-5.8 12.1-8.9 18.3-8.9 7.6 0 13.8 4.7 17.7 8.9l60.3 60.2c12 12 11.9 25-.3 37.7-4.2 4.5-8.6 8.8-13.3 13.3-7 6.8-14.3 13.8-20.9 21.7-11.5 12.4-25.2 18.2-42.9 18.2-1.7 0-3.5-.1-5.2-.2-32.8-2.1-63.3-14.9-86.2-25.8-62.2-30.1-116.8-72.8-162.1-127-37.3-44.9-62.4-86.7-79-131.5-10.3-27.5-14.2-49.6-12.6-69.7z"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+                <a href="javascript:void(0)" className="text-current text-sm ml-3">
+                  <strong>+92 (333) 1234567</strong>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="z-10 relative h-full max-md:min-h-[350px]">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3448.0986337238387!2d67.0330896!3d30.2057322!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ed2de51c5020c55%3A0x78c544f5c63c4e3e!2sQuetta%20Club%20Limited!5e0!3m2!1sen!2s!4v1707723788558!5m2!1sen!2s"
+              className="left-0 top-0 h-full w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg"
+              frameborder="0"
+              allowfullscreen
+            ></iframe>
           </div>
         </div>
       </Container>
