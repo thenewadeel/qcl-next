e208c7aa7b164d36998292e95c073e937fc35ea2
Ad <thenewadeel@gmail.com>
2024-06-23 02:19:42 +0500

    Build optimization failed: found page without a React Component as default export in 2:13:29 AM: pages/app/appwrite . . .

--- pages/app/appwrite.js
+++ pages/app/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Account, Databases, Models } from "appwrite";
+import { Client, Account, Databases, Storage } from "appwrite";
 
 export const client = new Client();
 
@@ -9,5 +9,12 @@ export { ID } from "appwrite";
 
 export const database = new Databases(client);
 
+export const storage = new Storage(client);
 // You then use the imported type definitions like this
 // const authUser = await account.createEmailPasswordSession(email, password);
+
+const appwrite = () => {
+  return <div className="flex flex-col self-center items-center border-2 border-red-600"></div>;
+};
+
+export default appwrite;
